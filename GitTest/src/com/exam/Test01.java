3128ff1395ad2e44af33362e879cc1c71e502152
smhrd <smhrd@211.63.240.88>
2021-11-10 15:42:33 +0900

    두 번째 커밋!

--- GitTest/src/com/exam/Test01.java
+++ GitTest/src/com/exam/Test01.java
@@ -5,6 +5,7 @@ public class Test01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ã¹ Ä¿¹Ô!");
+		System.out.println("µÎ¹øÂ° Ä¿¹Ô!");
 	}
 
 }
