eb1597d980fca38c36408d2e8df14305f5004602
smhrd <smhrd@220.93.229.232>
2021-11-18 12:50:54 +0900

    지나1118.1

---
+++ GitTest/src/DAO/CommunityDAO.java
@@ -0,0 +1,132 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Date;
+
+import DTO.CommunityDTO;
+
+public class CommunityDAO {
+	PreparedStatement psmt=null;
+	Connection conn=null;
+	ResultSet rs=null;
+	int cnt=0;
+	CommunityDTO dto=null;
+	//DB연결 메소드
+	public void getConn() {
+		try {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		
+		//카드키
+		String db_url="jdbc:oracle:thin:@project-db-stu.ddns.net:1524:xe";
+		String db_id="campus_c_c_1111";
+		String db_pw="smhrd3";
+		
+		//연결
+		 conn= DriverManager.getConnection(db_url,db_id,db_pw);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	//DB종료 메소드
+	 public void dbClose() {
+		 try {
+				if(rs!=null) rs.close();
+				if(psmt!=null) psmt.close();
+				if(conn!=null) conn.close();
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+	 }
+	 
+	//파일 업로드 메소드
+		 public int upload(CommunityDTO dto) {
+			 getConn();
+			 try {
+				 String sql="insert into t_community values(article_seq.nextval,?,?,?,?,?,?,sysdate,?)";
+				 
+				 psmt=conn.prepareStatement(sql);
+				 
+				 psmt.setString(1,dto.getArticle_title());
+				 psmt.setString(2,dto.getArticle_content());
+				 psmt.setString(3,dto.getMb_id());
+				 psmt.setString(4,dto.getArticle_file1());
+				 psmt.setString(5,dto.getArticle_file2());
+				 psmt.setString(6,dto.getArticle_file3());
+				 psmt.setInt(7,dto.getArticle_cnt());
+				
+				 cnt=psmt.executeUpdate();
+				 
+			 }catch(Exception e){
+					e.printStackTrace();
+				}finally {
+					dbClose();
+				}return cnt;
+		 }
+		 
+		 //게시글 보여주는 메소드
+		 public ArrayList<CommunityDTO> viewBoard() {
+			 
+			 ArrayList<CommunityDTO> b_list = new ArrayList<>();
+			 getConn();
+			 
+			 try {
+				 String sql="select*from t_communitty order by reg_date desc";
+				 
+				 psmt=conn.prepareStatement(sql);
+				 rs=psmt.executeQuery();
+				 
+				 while(rs.next()) {
+					 int article_seq=rs.getInt("article_seq");
+					String article_title= rs.getString("article_title");
+					String article_content= rs.getString("article_content");
+					String mb_id= rs.getString("mb_id");
+					String article_file1= rs.getString("article_file1");
+					String article_file2= rs.getString("article_file2");
+					String article_file3= rs.getString("article_file3");
+					Date reg_date= rs.getDate("reg_date");
+					int article_cnt=rs.getInt("article_cnt");
+					
+					dto= new CommunityDTO(article_seq,article_title,article_content,mb_id,article_file1,article_file2,article_file3,reg_date,article_cnt);
+					b_list.add(dto);
+				 }
+				 
+			 }catch(Exception e){
+					e.printStackTrace();
+				}finally {
+					dbClose();
+				}return b_list;
+		 }
+		 
+		 //게시글 세부내용을 보여주는 메소드
+		 public CommunityDTO viewOneBoard(String num) {
+			 getConn();
+			 
+			 try {
+				 String sql="select * from t_community where num=?";
+				 
+				 psmt=conn.prepareStatement(sql);
+				 psmt.setString(1,num);
+				 rs=psmt.executeQuery();
+				 
+				 if(rs.next()) {
+				int num2= rs.getInt("num");
+				String title= rs.getString("title");
+				String writer= rs.getString("writer");
+				String fileName= rs.getString("fileName");
+				String content= rs.getString("content");
+				String b_date= rs.getString("b_date");
+				
+			  dto=new CommunityDTO();
+				 }
+			 }catch(Exception e){
+					e.printStackTrace();
+				}finally {
+					dbClose();
+				}return dto;
+		 }
+}
---
+++ GitTest/src/DTO/CommunityDTO.java
@@ -0,0 +1,85 @@
+package DTO;
+
+import java.util.Date;
+
+public class CommunityDTO {
+
+	private int article_seq;
+	private String article_title;
+	private String article_content;
+	private String mb_id;
+	private String article_file1;
+	private String article_file2;
+	private String article_file3;
+	private Date reg_date;
+	private int article_cnt;
+	
+	public CommunityDTO(int article_seq, String article_title, String article_content, String mb_id,
+			String article_file1, String article_file2, String article_file3, Date reg_date, int article_cnt) {
+		
+		this.article_seq = article_seq;
+		this.article_title = article_title;
+		this.article_content = article_content;
+		this.mb_id = mb_id;
+		this.article_file1 = article_file1;
+		this.article_file2 = article_file2;
+		this.article_file3 = article_file3;
+		this.reg_date = reg_date;
+		this.article_cnt = article_cnt;
+	}
+	public int getArticle_seq() {
+		return article_seq;
+	}
+	public void setArticle_seq(int article_seq) {
+		this.article_seq = article_seq;
+	}
+	public String getArticle_title() {
+		return article_title;
+	}
+	public void setArticle_title(String article_title) {
+		this.article_title = article_title;
+	}
+	public String getArticle_content() {
+		return article_content;
+	}
+	public void setArticle_content(String article_content) {
+		this.article_content = article_content;
+	}
+	public String getMb_id() {
+		return mb_id;
+	}
+	public void setMb_id(String mb_id) {
+		this.mb_id = mb_id;
+	}
+	public String getArticle_file1() {
+		return article_file1;
+	}
+	public void setArticle_file1(String article_file1) {
+		this.article_file1 = article_file1;
+	}
+	public String getArticle_file2() {
+		return article_file2;
+	}
+	public void setArticle_file2(String article_file2) {
+		this.article_file2 = article_file2;
+	}
+	public String getArticle_file3() {
+		return article_file3;
+	}
+	public void setArticle_file3(String article_file3) {
+		this.article_file3 = article_file3;
+	}
+	public Date getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Date reg_date) {
+		this.reg_date = reg_date;
+	}
+	public int getArticle_cnt() {
+		return article_cnt;
+	}
+	public void setArticle_cnt(int article_cnt) {
+		this.article_cnt = article_cnt;
+	}
+	
+}
