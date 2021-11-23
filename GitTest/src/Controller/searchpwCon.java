d866c607b9baead924f109f5867087b09341d785
smhrd <smhrd@222.102.104.14>
2021-11-23 17:26:42 +0900

    흠~

--- GitTest/src/Controller/searchpwCon.java
+++ GitTest/src/Controller/searchpwCon.java
@@ -39,8 +39,7 @@ public class searchpwCon extends HttpServlet {
 		System.out.println("mb_id : " + id);
 		System.out.println("mb_birthdate : " + birthdate);
 		System.out.println("mb_phone : " +phone );
-		
-<<<<<<< HEAD
+
 		
 		response.setContentType("text/html; charset=UTF-8");
 
@@ -51,9 +50,9 @@ public class searchpwCon extends HttpServlet {
 		writer.close();
 		
 		//response.sendRedirect("./Resources/index.jsp");
-=======
+
 		response.sendRedirect("./Resources/index.html");
->>>>>>> branch 'master' of https://github.com/2021-SMHRD-KDT-AI-7/SOP.git
+
 		
 	}
 
